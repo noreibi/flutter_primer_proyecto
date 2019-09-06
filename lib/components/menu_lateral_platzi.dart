7912aa97917cd0580f96f64910e089d581c7325c
noreibi <noreibileal@gmail.com>
2019-09-05 21:23:39 -0300

    Menu Lateral
    Esto dibuja un DrawerMenu al estilo de Android, muy lindo

---
+++ lib/components/menu_lateral_platzi.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+class MenuLateralplatzi extends StatelessWidget { 
+  
+@override
+Widget build (BuildContext context){
+  return new Drawer(
+    child: ListView(
+      children: <Widget>[
+        new UserAccountsDrawerHeader(
+          accountName: Text("Noreibi"),
+          accountEmail: Text("noreibileal@gmail.com"),
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: NetworkImage('https://i.pinimg.com/originals/20/f7/8d/20f78d4fa88e410b775e4cd4d911f7ef.jpg'),
+          fit: BoxFit.cover,
+          ),
+          )
+        ),
+          new ListTile(
+            title: Text("MENU 1"),
+          ),
+          new ListTile(
+            title: Text("MENU 2"),
+          ),
+          new ListTile(
+            title: Text("MENU 3"),
+          ),
+          new ListTile(
+            title: Text("MENU 4"),
+          )
+            ],
+    )
+  );
+}
+                     
+              
+
+Image dibujarBackground(){
+  return Image.network(
+          'http://replica-animal.com/images/stories/aves-posadas/lechuza-comun.jpg',
+          fit: BoxFit.fill,
+          height: double.maxFinite,
+          width: double.maxFinite,
+        );
+}
+
+Center dibujarCintaNetra(){
+return Center(
+  child: Container(
+  height: 50,
+  decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              stops: [0.5, 0.5],
+              colors: [
+                Color.fromRGBO(0, 0, 0, 0.2),
+                Color.fromRGBO(0, 0, 0, 0.5),
+              ],
+            ),
+          ),
+),
+);
+}
+
+Center dibujarMensaje(){
+return Center(
+  child: Text("#LaLechuza hace chu", style: TextStyle(color: Colors.white), textAlign: TextAlign.center,),
+);
+}
+
+}
+
+
+
